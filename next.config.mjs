5dd93c1493318475a5e652836f0e48f2d2c663f5
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2024-09-03 05:44:32 +0300

    theme colors

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  experimental: {
+  /* experimental: {
     instrumentationHook: true,
-  },
+  },*/
   async headers() {
     return [
       {
