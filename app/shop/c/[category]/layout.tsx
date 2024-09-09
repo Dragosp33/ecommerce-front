6851723830b85ff1b58466dcf7afbec0a6365874
Dragosp33 <dragos.polifronie@s.unibuc.ro>
2024-09-09 08:47:19 +0300

    fixed category-filters

--- app/shop/c/[category]/layout.tsx
+++ app/shop/c/[category]/layout.tsx
@@ -11,18 +11,19 @@ export default async function layout({
 }>) {
   console.log('LAYOUT CALLED');
   const category = params.category || '1';
-  const res = await fetch(
-    `http://admin.shop.localhost:3001/api/category-filter/${category}`,
-    {
-      method: 'GET',
-      next: { revalidate: 10 },
-    }
-  );
+  let url =
+    `${process.env.NEXT_PUBLIC_ADMIN_DOMAIN_URL}/api/category-filter/${category}` ||
+    `http://admin.shop.localhost:3001/api/category-filter/${category}`;
+  const res = await fetch(url, {
+    method: 'GET',
+    next: { revalidate: 10 },
+  });
   const filters = await res.json();
   console.log('FILTERS:::: ', filters, filters.properties);
+
   return (
     <>
-      <Filters filters={filters.properties} />
+      {filters.properties && <Filters filters={filters.properties} />}
       {children}
     </>
   );
